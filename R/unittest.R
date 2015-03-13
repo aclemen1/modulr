0fc97496eaea9aa66edff28adc9d31414dd6b1eb
Alain Clément-Pavon <alain.clement-pavon@unil.ch>
2015-03-13 12:55:30 +0100

    Add exit status to run_tests()

--- R/unittest.R
+++ R/unittest.R
@@ -11,6 +11,8 @@ run_tests <- function() {
   runit_options$verbose = 0
   options("RUnit" = runit_options)
   suites <- paths_config$get_all()
+  errors <- 0
+  failures <- 0
   for(suite_name in names(suites)) {
     suite_path <- suites[[suite_name]]
     test_suite <- defineTestSuite(
@@ -22,6 +24,9 @@ run_tests <- function() {
       testFileRegexp = ".*[^_]\\.R$",
       testFuncRegexp = "^test\\..+")
     test_result <- runTestSuite(test_suite)
+    errors <- errors + test_result[[suite_name]]$nErr
+    failures <- failures + test_result[[suite_name]]$nFail
     printTextProtocol(test_result)
   }
+  if(errors + failures) stop(.call = F)
 }
