4c53442d7b8ed64de3c78f99d1f093109ed2898f
Alain Clément-Pavon <alain.clement-pavon@unil.ch>
2015-06-30 16:23:18 +0200

    Chord Diagram for register_graph()

--- R/graph.R
+++ R/graph.R
@@ -1,10 +1,24 @@
+#' Adjacency Matrix
+#'
+#' @export
+deps_matrix <- function() {
+  table(Reduce(rbind, Map(function(module) {
+    deps <- unlist(module$dependencies)
+    data.frame(from=rep(module$name, length(deps)), to=deps)
+  }, .internals()$register)))
+}
+
 #' Register graph
 #'
 #' @export
 register_graph <- function() {
-  adj <- Reduce(rbind, Map(function(module) {
-    deps <- unlist(module$dependencies)
-    data.frame(from=rep(module$name, length(deps)), to=deps)
-  }, .internals()$register))
-  graph.data.frame(adj)
+  deps <- deps_matrix()
+  chordDiagram(deps, annotationTrack = "grid", preAllocateTracks = list(track.height = 0.3))
+  circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+    xlim = get.cell.meta.data("xlim")
+    ylim = get.cell.meta.data("ylim")
+    sector.name = get.cell.meta.data("sector.index")
+    circos.text(mean(xlim), ylim[1], sector.name, facing = "clockwise",
+                niceFacing = TRUE, adj = c(0, 0.5))
+  }, bg.border = NA)
 }
