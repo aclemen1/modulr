50ef45a709552d79f5eecd2d8c585c7ec93f28c0
Alain Clément-Pavon <alain.clement-pavon@unil.ch>
2015-08-17 14:16:02 +0200

    purge unused system.time() call

--- R/instance.R
+++ R/instance.R
@@ -54,30 +54,28 @@ instanciate <- function(name, debug = F, force = F) {
               message_meta(sprintf("instanciating [%s] ...",
                                    ordered_name),
                            level = 3)
-          specs <- system.time({
-            env = new.env()
-            assign(".__name__", ordered_name, pos = env)
-            if(length(module$dependencies) > 0) {
-              args <- lapply(module$dependencies,
-                             function(name) register[[name]]$instance)
-              # tricky bug solution, see below
-              module$instance <- evalq(do.call(
-                eval(parse(text=deparse(module$factory))),
-                args = args), envir = env)
-            } else {
-              # the deparse %>% parse %>% eval trick solves the following bug
-              # WORKS:
-              # module$instance <- evalq(do.call(function() {get("variable", pos = env)}, args = list()), envir = env)
-              # DOES NOT WORK:
-              # f <- function() {get("variable", pos = env)}
-              # module$instance <- evalq(do.call(f, args = list()), envir = env)
-              # WORKAROUND:
-              # module$instance <- evalq(do.call(eval(parse(text=deparse(f))), args = list()), envir = env)
-              module$instance <- evalq(do.call(
-                eval(parse(text=deparse(module$factory))),
-                args = list()), envir = env)
-            }
-          })
+          env = new.env()
+          assign(".__name__", ordered_name, pos = env)
+          if(length(module$dependencies) > 0) {
+            args <- lapply(module$dependencies,
+                           function(name) register[[name]]$instance)
+            # tricky bug solution, see below
+            module$instance <- evalq(do.call(
+              eval(parse(text=deparse(module$factory))),
+              args = args), envir = env)
+          } else {
+            # the deparse %>% parse %>% eval trick solves the following bug
+            # WORKS:
+            # module$instance <- evalq(do.call(function() {get("variable", pos = env)}, args = list()), envir = env)
+            # DOES NOT WORK:
+            # f <- function() {get("variable", pos = env)}
+            # module$instance <- evalq(do.call(f, args = list()), envir = env)
+            # WORKAROUND:
+            # module$instance <- evalq(do.call(eval(parse(text=deparse(f))), args = list()), envir = env)
+            module$instance <- evalq(do.call(
+              eval(parse(text=deparse(module$factory))),
+              args = list()), envir = env)
+          }
           module$instanciated <- T
           module$first_instance <- F
           register[[ordered_name]] <- module
