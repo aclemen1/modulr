8cf11db25d2ed1bf49fbfb6d843294255accef70
Alain Clément-Pavon <alain.clement-pavon@unil.ch>
2015-11-07 02:51:47 +0100

    Build vignettes with Travis CI and CRAN, but don't try to use webshot.

--- tests/testthat/test_lints.R
+++ tests/testthat/test_lints.R
@@ -2,6 +2,8 @@ if (requireNamespace("lintr", quietly = TRUE)) {
   context("lints")
   test_that("Package Style", {
     #skip("TODO: remove this skip") # Exclude Linting
+    testthat::skip_on_cran()
+    testthat::skip_on_travis()
     lintr::expect_lint_free()
   })
 }
