84ca09aad31f3b0e14b2a43562e8706e4a1c3eb4
Alain Clément-Pavon <alain.clement-pavon@unil.ch>
2016-02-23 22:36:35 +0000

    Ignore .Rprofile and .modulr_profile.R from git repository.

--- .Rprofile
+++
@@ -1,2 +0,0 @@
-options(repos = c(CRAN = "http://cran.r-project.org"))
-invisible(Sys.setlocale("LC_TIME", "en_DK.UTF-8"))
--- .modulr_profile.R
+++
@@ -1,6 +0,0 @@
-# paths_config$set(
-#   list(
-#     "modulr" = "vendor/modulr",
-#     "aclemen1" = "vendor/aclemen1"
-#     )
-#   )
