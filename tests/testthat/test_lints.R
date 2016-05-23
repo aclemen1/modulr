836968fa446d9e11c7a7b2e34035bebffff2a9d7
Alain Clément-Pavon <alain.clement-pavon@unil.ch>
2016-05-23 21:05:25 +0200

    Fix linting dependencies.

--- tests/testthat/test_lints.R
+++ tests/testthat/test_lints.R
@@ -3,18 +3,6 @@ if (requireNamespace("lintr", quietly = TRUE)) {
   test_that("Package Style", {
     testthat::skip_on_cran()
     testthat::skip_on_travis()
-    # Since testthat-0.11.0.9000, lintr::expect_lint_free is not working
-    # TODO adapt when lintr is updated
-    if (packageVersion("testthat") >= package_version("0.11.0.9000")) {
-      tryCatch({
-        lintr::expect_lint_free()
-      },
-      error = function(e) {
-        skip(paste("Skipping linting with testthat >= 0.11.0.9000.",
-                   "Should be fixed in a future release of lintr."))
-      })
-    } else {
-      lintr::expect_lint_free()
-    }
+    lintr::expect_lint_free()
   })
 }
